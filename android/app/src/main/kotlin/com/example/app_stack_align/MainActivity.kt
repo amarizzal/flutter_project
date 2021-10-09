3797a31c11a5ae318c59a5807fb83fcc03c8d027
RIZAL AMMAR <rizal.ammar91@yahoo.com>
2021-10-10 01:01:22 +0700

    persiapan push

---
+++ android/app/src/main/kotlin/com/example/app_stack_align/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_stack_align
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Stack dan Align"),
+      ),
+      body: Stack(
+        children: [
+          // background
+          Column(
+            children: [
+              Flexible(
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: Container(color: Colors.grey),
+                      flex: 1,
+                    ),
+                    Flexible(
+                      child: Container(color: Colors.white),
+                      flex: 1,
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: Container(color: Colors.white),
+                      flex: 1,
+                    ),
+                    Flexible(
+                      child: Container(color: Colors.grey),
+                      flex: 1,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          // Listview
+          ListView(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Ini adalah teks yang ada ditengah dari stack",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+          // button
+        ],
+      ),
+    ));
+  }
+}
